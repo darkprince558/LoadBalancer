a2ddfe2beff3e7441c8a23c1cee524ec3aca25cd
Anish Jami <jamianish8@gmail.com>
2025-11-30 16:28:01 -0500

    Adding HealthCheck to keep track of what servers are up and are down

--- src/LoadBalancer.java
+++ src/LoadBalancer.java
@@ -6,12 +6,18 @@ import java.util.*;
 public class LoadBalancer {
     private static final int Listen_PORT = 8080;
     private static final String BACKEND_HOST = "localhost";
-    private static final Vector<Integer> SERVERS = new Vector<>(Arrays.asList(9001, 9002));
+
+    private static final List<Integer> SERVERS_LIST = Arrays.asList(9001, 9002);
+    private static final Vector<Integer> LIVE_SERVERS = new Vector<>(SERVERS_LIST);
+
+    private static final int HEALTH_CHECK_TIME = 500;
 
     static void main() throws IOException {
         ServerSocket serverSocket = new ServerSocket(Listen_PORT);
+        startHealthCheck();
+
         System.out.println("Load Balancer started on port " + Listen_PORT);
-        System.out.println("Balancing traffic between ports: " + SERVERS);
+        System.out.println("Balancing traffic between ports: " + LIVE_SERVERS);
 
         int currentServerIndex = 0;
 
@@ -25,33 +31,72 @@ public class LoadBalancer {
 
 
 
-            while (backendSocket == null && attempts < SERVERS.size()) {
-                if (SERVERS.isEmpty()) {
-                    System.out.println("No servers available");
+            while (true) {
+                if (LIVE_SERVERS.isEmpty()) {
+                    System.out.println("503 Service Unavailable: No live servers.");
+                    clientSocket.close();
+                    continue;
                 }
 
-                int backendPort = SERVERS.get(currentServerIndex);
-                currentServerIndex = (currentServerIndex + 1) % SERVERS.size();
+                // Fix index if it went out of bounds (because the list shrank)
+                if (currentServerIndex >= LIVE_SERVERS.size()) currentServerIndex = 0;
+
+
+                int backendPort = LIVE_SERVERS.get(currentServerIndex);
+                currentServerIndex = (currentServerIndex + 1) % LIVE_SERVERS.size();
 
                 try {
                     backendSocket = new Socket(BACKEND_HOST, backendPort);
+                    Thread clientToBackend = new Thread(new StreamPipe(clientSocket.getInputStream(), backendSocket.getOutputStream()));
+                    Thread backendToClient = new Thread(new StreamPipe(backendSocket.getInputStream(), clientSocket.getOutputStream()));
+                    clientToBackend.start();
+                    backendToClient.start();
+                    System.out.println("Forwarding to " + backendSocket.getPort());
                 } catch (IOException e) {
-                    System.err.println("Server on port " + backendPort + " is dead. Removing from rotation.");
-                    SERVERS.remove(Integer.valueOf(backendPort));
-
-                    if (currentServerIndex >= SERVERS.size()) currentServerIndex = 0;
+                    System.err.println("Connection failed to " + backendPort);
+                    clientSocket.close();
                 }
                 attempts++;
 
             }
+        }
+    }
 
-            if (backendSocket != null) {
-                System.out.println("Forwarding to " + backendSocket.getPort());
-                Thread clientToBackend = new Thread(new StreamPipe(clientSocket.getInputStream(), backendSocket.getOutputStream()));
-                Thread backendToClient = new Thread(new StreamPipe(backendSocket.getInputStream(), clientSocket.getOutputStream()));
-                clientToBackend.start();
-                backendToClient.start();
+    public static void startHealthCheck(){
+        Thread healthCheckThread = new Thread(() ->
+        {
+            while(true){
+                try {
+                    Thread.sleep(HEALTH_CHECK_TIME);
+                    for (int port: SERVERS_LIST){
+                        if(isServerAlive(port)){
+                            if (!LIVE_SERVERS.contains(port)){
+                                System.out.println(">>> Server " + port + " is BACK ONLINE! Adding to rotation.");
+                                LIVE_SERVERS.add(port);}
+                            else {
+                                if (LIVE_SERVERS.contains(port)) {
+                                    System.out.println(">>> Server " + port + " is DOWN. Removing from rotation.");
+                                    LIVE_SERVERS.remove(Integer.valueOf(port));
+                                }
+                            }
+                        }
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
+        }
+        );
+        healthCheckThread.start();
+    }
+
+    private static boolean isServerAlive(int port){
+        try {
+            Socket socket = new Socket(BACKEND_HOST, port);
+            socket.close();
+            return true;
+        } catch (IOException e) {
+            return false;
         }
     }
 }
