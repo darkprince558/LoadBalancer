35c39988477fef261a2a4c3f4c6c5f04d6ebec42
Anish Jami <jamianish8@gmail.com>
2025-11-30 14:21:04 -0500

    Initial implementation of TCP proxy and bi directional piping

--- src/LoadBalancer.java
+++ src/LoadBalancer.java
@@ -1,4 +1,35 @@
-package PACKAGE_NAME;
+import java.io.*;
+import java.net.*;
+import java.nio.channels.Pipe;
 
 public class LoadBalancer {
-}
+    private static final int Listen_PORT = 8080;
+    private static final String BACKEND_HOST = "localhost";
+    private static final int BACKEND_PORT = 9001;
+
+    static void main() throws IOException {
+        ServerSocket serverSocket = new ServerSocket(Listen_PORT);
+        System.out.println("Server started on port " + Listen_PORT);
+
+        while (true) {
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("Client connected");
+
+            // connected to a backend server
+            Socket backendSocket = new Socket(BACKEND_HOST, BACKEND_PORT);
+
+            // Client to backend
+            Thread clientToBackend = new Thread(new StreamPipe(clientSocket.getInputStream(), backendSocket.getOutputStream()));
+
+            // Backend to client
+            Thread backendToClient = new Thread(new StreamPipe(backendSocket.getInputStream(), clientSocket.getOutputStream()));
+
+            clientToBackend.start();
+            backendToClient.start();
+
+        }
+        }
+
+
+
+    }
---
+++ src/StreamPipe.java
@@ -0,0 +1,28 @@
+import java.io.*;
+
+public class StreamPipe implements Runnable {
+    private final InputStream input;
+    private final OutputStream output;
+
+    public StreamPipe(InputStream input, OutputStream output) {
+        this.input = input;
+        this.output = output;
+    }
+
+    @Override
+    public void run() {
+        try {
+            // 4kb to carry packets
+            byte[] buffer = new byte[4096];
+            int bytesRead;
+
+            // keep reading until the stream closes
+            while ((bytesRead = input.read(buffer)) != -1) {
+                output.write(buffer, 0, bytesRead);
+                output.flush(); //force the data through immediately
+            }
+        } catch (IOException e) {
+            System.out.println("Stream closed.");
+        }
+    }
+}
