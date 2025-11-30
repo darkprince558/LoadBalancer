76bb4e3fb6b12e3d74631e0280754ff108e1c2d4
Anish Jami <jamianish8@gmail.com>
2025-11-30 15:15:05 -0500

    adding features to get rid of dead servers from the vector

--- src/LoadBalancer.java
+++ src/LoadBalancer.java
@@ -6,12 +6,11 @@ import java.util.*;
 public class LoadBalancer {
     private static final int Listen_PORT = 8080;
     private static final String BACKEND_HOST = "localhost";
-    private static final List<Integer> SERVERS = Arrays.asList(9001, 9002);
-
+    private static final Vector<Integer> SERVERS = new Vector<>(Arrays.asList(9001, 9002));
 
     static void main() throws IOException {
         ServerSocket serverSocket = new ServerSocket(Listen_PORT);
-        System.out.println("Server started on port " + Listen_PORT);
+        System.out.println("Load Balancer started on port " + Listen_PORT);
         System.out.println("Balancing traffic between ports: " + SERVERS);
 
         int currentServerIndex = 0;
@@ -21,29 +20,38 @@ public class LoadBalancer {
             Socket clientSocket = serverSocket.accept();
             System.out.println("Client connected");
 
-            // connected to a server using round robin
-            int backendPort = SERVERS.get(currentServerIndex);
-            System.out.println("Request received. Forwarding to Server on Port: " + backendPort);
-            currentServerIndex = (currentServerIndex + 1) % SERVERS.size();
+            Socket backendSocket = null;
+            int attempts = 0;
 
 
-            try {
-                Socket backendSocket = new Socket(BACKEND_HOST, backendPort);
 
-                // Start the Pipes
+            while (backendSocket == null && attempts < SERVERS.size()) {
+                if (SERVERS.isEmpty()) {
+                    System.out.println("No servers available");
+                }
+
+                int backendPort = SERVERS.get(currentServerIndex);
+                currentServerIndex = (currentServerIndex + 1) % SERVERS.size();
+
+                try {
+                    backendSocket = new Socket(BACKEND_HOST, backendPort);
+                } catch (IOException e) {
+                    System.err.println("Server on port " + backendPort + " is dead. Removing from rotation.");
+                    SERVERS.remove(Integer.valueOf(backendPort));
+
+                    if (currentServerIndex >= SERVERS.size()) currentServerIndex = 0;
+                }
+                attempts++;
+
+            }
+
+            if (backendSocket != null) {
+                System.out.println("Forwarding to " + backendSocket.getPort());
                 Thread clientToBackend = new Thread(new StreamPipe(clientSocket.getInputStream(), backendSocket.getOutputStream()));
                 Thread backendToClient = new Thread(new StreamPipe(backendSocket.getInputStream(), clientSocket.getOutputStream()));
-
                 clientToBackend.start();
                 backendToClient.start();
-
-            } catch (IOException e) {
-                // If a backend server is dead, this prevents the Load Balancer from crashing
-                System.err.println("Failed to connect to backend port " + backendPort);
-                clientSocket.close();
             }
         }
-
-
     }
 }
