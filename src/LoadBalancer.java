20f349051e45cb1ad112c6856eda184e6610a4b6
Anish Jami <jamianish8@gmail.com>
2025-11-30 14:55:11 -0500

    Implement round robin algorithm

--- src/LoadBalancer.java
+++ src/LoadBalancer.java
@@ -1,31 +1,41 @@
 import java.io.*;
 import java.net.*;
-import java.nio.channels.Pipe;
+import java.util.*;
+
 
 public class LoadBalancer {
     private static final int Listen_PORT = 8080;
     private static final String BACKEND_HOST = "localhost";
-    private static final int BACKEND_PORT = 9001;
+    private static final List<Integer> SERVERS = Arrays.asList(9001, 9002);
+
 
     static void main() throws IOException {
         ServerSocket serverSocket = new ServerSocket(Listen_PORT);
         System.out.println("Server started on port " + Listen_PORT);
+        System.out.println("Balancing traffic between ports: " + SERVERS);
+
+        int currentServerIndex = 0;
+
 
         while (true) {
             Socket clientSocket = serverSocket.accept();
             System.out.println("Client connected");
 
-            // connected to a backend server
-            Socket backendSocket = new Socket(BACKEND_HOST, BACKEND_PORT);
+            // connected to a server using round robin
+            int backendPort = SERVERS.get(currentServerIndex);
+            System.out.println("Request received. Forwarding to Server on Port: " + backendPort);
+            currentServerIndex = (currentServerIndex + 1) % SERVERS.size();
 
-            // Client to backend
-            Thread clientToBackend = new Thread(new StreamPipe(clientSocket.getInputStream(), backendSocket.getOutputStream()));
 
-            // Backend to client
-            Thread backendToClient = new Thread(new StreamPipe(backendSocket.getInputStream(), clientSocket.getOutputStream()));
+            try {
+                Socket backendSocket = new Socket(BACKEND_HOST, backendPort);
 
-            clientToBackend.start();
-            backendToClient.start();
+                // Start the Pipes
+                Thread clientToBackend = new Thread(new StreamPipe(clientSocket.getInputStream(), backendSocket.getOutputStream()));
+                Thread backendToClient = new Thread(new StreamPipe(backendSocket.getInputStream(), clientSocket.getOutputStream()));
+
+                clientToBackend.start();
+                backendToClient.start();
 
             } catch (IOException e) {
                 // If a backend server is dead, this prevents the Load Balancer from crashing
@@ -35,5 +45,5 @@ public class LoadBalancer {
         }
 
 
-
     }
+}
